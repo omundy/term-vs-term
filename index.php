67797f4ebcce1ba4aebf8a447f41ae04f2d7d81a
Owen Mundy <omundy@gmail.com>
2022-08-30 16:18:54 -0400

    Switch to bootstrap cdn

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (isset($_GET['q1']) && isset($_GET['q2'])) {
 }
 ?></title>
 
-<link href="assets/libs/bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
 <link href="assets/css/styles.css" rel="stylesheet">
 
@@ -213,7 +213,7 @@ if ($q2['score'] == $q1['score']) {
 
 
 <script src="assets/libs/jquery-3.6.0.min.js"></script>
-<script src="assets/libs/bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <script src="assets/js/main.js"></script>
 
 <?php include("../../_site/partials/stats.php"); ?>
